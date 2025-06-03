16d9376836d92525c60378f2485a3f8b98fea32b
lucasdiaas79 <ldiascontato@gmail.com>
2025-06-03 12:36:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def visao_geral():
         st.session_state.confirm_delete = None
 
     for row_num, (i, row) in enumerate(df[::-1].iterrows()):
-        linha_sheet = df.index[::-1][row_num] + 2
+        linha_sheet = len(valores) - row_num
 
         with st.expander(f"👤 {row['cliente']}"):
             st.markdown(f"""
